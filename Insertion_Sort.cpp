89be053e87c21141cc84c57c3acf3dd6fd7b7d17
DimasCW <cdimas292@gmail.com>
2023-03-24 10:36:02 +0700

    menambahkan perulangan di line 26

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -4,8 +4,7 @@
 #include <iostream>
 using namespace std;
 
-int arr[20];
-int n
+int arr[20], n;
 
 void input() {
     while (true) {
@@ -23,6 +22,12 @@ void input() {
     cout << "================================" << endl;
     cout << "Masukkan Element Array" << endl;
     cout << "================================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data-ke- " << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+
 }
 
 int main()
