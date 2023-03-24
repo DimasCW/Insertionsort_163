11347299ecdd73244e1f84c629e3506f3d5d9b01
DimasCW <cdimas292@gmail.com>
2023-03-24 11:04:52 +0700

    menambahkan pengulangan

--- Insertion_Sort.cpp
+++ Insertion_Sort.cpp
@@ -58,6 +58,11 @@ void display() {
     cout << "\n========================" << endl;
     cout << "Element Array yang tela tersusun" << endl;
     cout << "==========================" << endl;
+
+    for (int j = 0; j < n; j++) {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
 }
 
 int main()
